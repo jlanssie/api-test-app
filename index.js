283d1e9207759136db91ad5f79d5f1457f2b2399
Jeremy Lanssiers <jlanssie@gmail.com>
2026-02-27 22:46:11 +0100

    update

---
+++ index.js
@@ -0,0 +1,64 @@
+import config from './config.json' with {type: 'json'};
+import data from './data.json' with {type: 'json'};
+
+if (config.security.insecure) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.removeAllListeners('warning');
+}
+
+async function run() {
+  const toObj = (val) => (val && typeof val === 'object' ? val : {});
+
+  const verbose = config.log.verbose || false;
+
+  for (const request of data.requests) {
+    const requestConfig = {
+      ...data.template,
+      ...request,
+      headers: {...toObj(data.template?.headers), ...toObj(request?.headers)},
+      queryParams: {...toObj(data.template?.queryParams), ...toObj(request?.queryParams)},
+      payload: {...toObj(data.template?.payload), ...toObj(request?.payload)}
+    };
+
+    const url = new URL(requestConfig.segment, requestConfig.domain);
+    Object.entries(requestConfig.queryParams || {}).forEach(([k, v]) => url.searchParams.append(k, v));
+
+    console.log(`📡 [${requestConfig.method}] ${url.href}`);
+
+    try {
+      const hasBody = !['GET', 'HEAD'].includes(requestConfig.method.toUpperCase()) && Object.keys(requestConfig.payload).length > 0;
+
+      await fetch(url.href, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: hasBody
+          ? JSON.stringify(requestConfig.payload)
+          : null
+      }).then(async (response) => {
+        if (response.ok) {
+          console.log(`✅ ${response.status}`);
+        } else {
+          console.error(`❌ ${response.status}`);
+        }
+
+        if (verbose) {
+          const responsePayload = await response.text();
+          let parsedResponsePayload = "";
+          try {
+            parsedResponsePayload = JSON.parse(responsePayload);
+          } catch {
+            parsedResponsePayload = responsePayload || '[Empty Response]';
+          }
+          console.log(parsedResponsePayload);
+        }
+      });
+
+    } catch (err) {
+      console.error(`❌ Error. ${err.message}`);
+    }
+
+    console.log('\n', '-'.repeat(10), '\n');
+  }
+}
+
+run().then(() => console.log("Done."));
